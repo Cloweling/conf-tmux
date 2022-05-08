828fd7ffe85a7f5dffceaa77bb3c49768fc150e7
Renato Ayau <renato.ayau@gmail.com>
2022-05-07 21:58:38 -0600

    config install

--- install.sh
+++ install.sh
@@ -1,39 +1,31 @@
 #! /bin/sh
-NAME_DIRECTORY_FILE="zsh"
+NAME_DIRECTORY_FILE="tmux"
 ROOT_PATH=~/.$NAME_DIRECTORY_FILE
 
 echo "Remove old files..."
 # Delete old directory
 [ -d $ROOT_PATH ] &&  rm -rf $ROOT_PATH
 
-# Delete old file zshrc
-[ -f ~/.zshrc ] && rm ~/.zshrc
-
-# Delete old file enviroment
-[ -f ~/.zshenv ] && rm ~/.zshenv
+# Delete old file tmux.conf
+[ -f ~/.tmux.conf ] && rm ~/.tmux.conf
 echo "Done!"
 
 # Download repository
 echo "Downloading repository..."
 cd /tmp
-wget -q https://github.com/Cloweling/conf-zsh/archive/refs/heads/main.zip -O zsh_tmp.zip
-unzip -q zsh_tmp.zip
-mv conf-zsh-main $ROOT_PATH
-rm zsh_tmp.zip
+wget -q https://raw.githubusercontent.com/Cloweling/conf-tmux/main/install.sh -O tmux_tmp.zip
+unzip -q tmux_tmp.zip
+mv conf-tmux-main $ROOT_PATH
+rm tmux_tmp.zip
 echo "Done!"
 
 # Install plugins
 echo "Installing plugins..."
 cd $ROOT_PATH
-git clone -q git@github.com:marlonrichert/zcolors.git plugins/zcolors
-git clone -q git@github.com:Aloxaf/fzf-tab.git plugins/fzf-tab
-git clone -q https://github.com/zsh-users/zsh-syntax-highlighting.git plugins/zsh-syntax-highlighting
-git clone -q https://github.com/zsh-users/zsh-autosuggestions.git plugins/zsh-autosuggestions
-git clone -q https://github.com/zsh-users/zsh-history-substring-search.git plugins/zsh-history-substring-search
+git clone -q https://github.com/tmux-plugins/tpm.git plugins/tpm
 echo "Done!"
 
 # install zshrc
 echo "Installing file zsh..."
-mv zshrc ~/.zshrc
-mv zshenv ~/.zshenv
+mv tmux.conf ~/.tmux.conf
 echo "Done!"
